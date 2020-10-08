7bfdc0e0f91939e242b2499c2328bd579ebae998
pengzhiming <717864916@qq.com>
2020-10-08 23:13:54 +0800

    使用input/emit完成响应

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,25 +1,19 @@
 <template>
   <div>
-    <Switch />
-    <!-- <div v-for="demoItem in demoArr" :key="demoItem">Switch 的文档</div> -->
+    <Switch :value="y" @input="y = $event" />
   </div>
 </template> 
 
 <script lang="ts">
 import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
 export default {
   components: {
     Switch
   },
   setup() {
-    let demoArr = [];
-    demoArr.length = 100;
-    for (let i = 0; i < demoArr.length; i++) {
-      demoArr[i] = i;
-    }
-    return {
-      demoArr
-    };
+    const y = ref(false);
+    return { y };
   }
 };
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{checked}">
+  <button @click="toggle" :class="{checked:value}">
     <span></span>
   </button>
 </template>
@@ -7,12 +7,14 @@
 <script lang="ts">
 import { ref } from "vue";
 export default {
-  setup() {
-    const checked = ref(false);
+  props: {
+    value: Boolean
+  },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value;
+      context.emit("input", !props.value);
     };
-    return { checked, toggle };
+    return { toggle };
   }
 };
 </script>
@@ -36,6 +38,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 button.checked {
   background: blue;
