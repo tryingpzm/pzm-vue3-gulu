c2a9f469424d7225e076905787ba26594cf75a1e
pengzhiming <717864916@qq.com>
2020-10-08 12:38:19 +0800

    基本页面完成

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="demoItem in demoArr" :key="demoItem">Button 的文档</div>
+  </div>
+</template> 
+
+<script lang="ts">
+export default {
+  setup() {
+    let demoArr = [];
+    demoArr.length = 100;
+    for (let i = 0; i < demoArr.length; i++) {
+      demoArr[i] = i;
+    }
+    return {
+      demoArr
+    };
+  }
+};
+</script>
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,4 +2,4 @@ declare module "*.vue" {
     import {ComponentOptions}  from 'vue'
     const componentOptions : ComponentOptions
     export default componentOptions
-} 
+}  
