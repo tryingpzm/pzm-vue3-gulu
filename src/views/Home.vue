48ad9351ad9f45fbfcc7ceb341eaa8533e14294d
pengzhiming <717864916@qq.com>
2020-10-08 10:40:57 +0800

    aside位置初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,21 +5,44 @@
         <h1>彭志明gulu</h1>
         <h2>Vue3小组件</h2>
         <p class="action">
-            <a href>Github</a>
-            <a href>开始</a>
+            <a href="https://github.com/tryingpzm/pzm-vue3-gulu" target="_blank">Github</a>
+            <router-link to="/doc">开始</router-link>
         </p>
     </div>
 </div>
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
     components: {
-        Topnav
-    }
-}
-</script> 
+        Topnav,
+    },
+};
+</script>
 
 <style lang="scss" scoped>
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+
+    >.actions {
+        padding: 8px 0;
+
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
+    }
+}
 </style>
