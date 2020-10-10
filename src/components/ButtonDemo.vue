70495d326352e455446d41fcc948f15a0e36fd5e
pengzhiming <717864916@qq.com>
2020-10-11 00:20:14 +0800

    支持size/theme

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,6 +5,12 @@
     <Button theme="text">按钮</Button>
     <Button theme="link">按钮</Button>
   </div>
+  <h1>示例2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
 </template> 
 
 <script lang="ts">
