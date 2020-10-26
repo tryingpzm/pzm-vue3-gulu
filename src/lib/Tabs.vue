752c070acf75441ade29ab04d3ab405901c6a70e
pengzhiming <717864916@qq.com>
2020-10-27 00:33:17 +0800

    feat: 优化tabs

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,7 +4,7 @@
       <div
         class="pzm-gulu-tabs-nav-item"
         v-for="(t,index) in titles"
-        :ref="el => { if (el) navItems[index] = el }"
+        :ref="el => { if (t===selected) selectedItem = el }"
         @click="select(t)"
         :class="{selected: t=== selected}"
         :key="index"
@@ -27,17 +27,15 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter(div => div.classList.contains("selected"))[0];
-      console.log(result);
-      const { width } = result.getBoundingClientRect();
+      if (!selectedItem.value) return;
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -68,7 +66,7 @@ export default {
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container
     };
