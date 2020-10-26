7d593730545c09ddbe6f5cb30343389ea4e6496b
pengzhiming <717864916@qq.com>
2020-10-27 00:22:25 +0800

    feat: tabs行为完成

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,35 +1,76 @@
 <template>
   <div class="pzm-gulu-tabs">
-    <div class="pzm-gulu-tabs-nav">
-      <div class="pzm-gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    <div class="pzm-gulu-tabs-nav" ref="container">
+      <div
+        class="pzm-gulu-tabs-nav-item"
+        v-for="(t,index) in titles"
+        :ref="el => { if (el) navItems[index] = el }"
+        @click="select(t)"
+        :class="{selected: t=== selected}"
+        :key="index"
+      >{{t}}</div>
+      <div class="pzm-gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="pzm-gulu-tabs-content">
-      <component
-        class="pzm-gulu-tabs-content-item"
-        v-for="(c,index) in defaults"
-        :is="c"
-        :key="index"
-      />
+      <component class="pzm-gulu-tabs-content-item" :is="current" :key="selected" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from "./Tab.vue";
+import { computed, ref, onMounted, onUpdated } from "vue";
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter(div => div.classList.contains("selected"))[0];
+      console.log(result);
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    onMounted(x);
+    onUpdated(x);
+
     const defaults = context.slots.default();
+
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
         throw new Error("Tabs 子标签必须是 Tab");
       }
     });
+    const current = computed(() => {
+      console.log("重新 return");
+      return defaults.filter(tag => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
     const titles = defaults.map(tag => {
       return tag.props.title;
     });
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
     return {
       defaults,
-      titles
+      titles,
+      current,
+      select,
+      navItems,
+      indicator,
+      container
     };
   }
 };
@@ -44,6 +85,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -55,9 +97,18 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
-  }
-  &-content {
-    padding: 8px 0;
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+    &-content {
+      padding: 8px 0;
+    }
   }
 }
 </style>
