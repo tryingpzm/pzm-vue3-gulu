589d38a59a48b774b1c1cb21e7e9fb32b6c64adc
pengzhiming <717864916@qq.com>
2020-10-26 23:38:15 +0800

    feat: tabs基础样式完成

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,20 +1,19 @@
 <template>
-  <div>
-    <div v-for="demoItem in demoArr" :key="demoItem">Tabs 的文档</div>
-  </div>
-</template> 
+  <div>Tabs 示例</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
+</template>
 
 <script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
 export default {
-  setup() {
-    let demoArr = [];
-    demoArr.length = 100;
-    for (let i = 0; i < demoArr.length; i++) {
-      demoArr[i] = i;
-    }
-    return {
-      demoArr
-    };
+  components: {
+    Tabs,
+    Tab
   }
 };
 </script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="pzm-gulu-tabs">
+    <div class="pzm-gulu-tabs-nav">
+      <div class="pzm-gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="pzm-gulu-tabs-content">
+      <component
+        class="pzm-gulu-tabs-content-item"
+        v-for="(c,index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach(tag => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是 Tab");
+      }
+    });
+    const titles = defaults.map(tag => {
+      return tag.props.title;
+    });
+    return {
+      defaults,
+      titles
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.pzm-gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
